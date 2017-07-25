e4aebc349a7f183648a8fa23b35a147354f9f88b
xb013 <ward.bret@gmail.com>
2017-07-25 07:43:43 -0700

    upload

---
+++ DHCP_Gen/Classes/Range.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHCP_Gen.Classes
+{
+    public class Range
+    {
+        private string _name;
+        private string _descr;
+        private int _min;
+        private int _max;
+        public Range():this(string.Empty,string.Empty,0,0) { }
+        public Range(string name, string descr, int min, int max) {
+            this._name = name;
+            this._descr = descr;
+            this._min = min;
+            this._max = max;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Description { get => _descr; set => _descr = value; }
+        public int Min { get => _min; set => _min = value; }
+        public int Max { get => _max; set => _max = value; }
+    }
+}
---
+++ DHCP_Gen/Form1.cs
@@ -0,0 +1,43 @@
+﻿using DHCP_Gen.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DHCP_Gen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void __writeXml() {
+            List<Range> r = new List<Range>();
+
+            r.Add(new Range("test1","test",0,5));
+
+            XmlSerializer ser = new XmlSerializer(r.GetType());
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tw = new XmlTextWriter(sw);
+            ser.Serialize(tw, r);
+            File.WriteAllText("test.xml", sw.ToString());
+            richTextBox1.AppendText(sw.ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            __writeXml();
+        }
+    }
+}
